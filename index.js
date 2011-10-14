1c865db8499d9367d9e296cd0a45af853a23fce3
Conner Petzold <cpetzold@gmail.com>
2011-10-13 23:57:29 -0700

    pwnbot is born

---
+++ index.js
@@ -0,0 +1,10 @@
+var irc = require('irc')
+  , bot = new irc.Client('irc.the0th.com', 'pwnbot', {
+      channels: ['#pwn']
+    , debug: true
+  });
+
+bot.on('join#pwn', function(who) {
+  bot.say('#pwn', who + ': pwned!');
+});
+
