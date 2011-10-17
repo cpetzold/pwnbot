12d7daa1117a0d2a26dc3016c0b067a8088b6344
Guillermo Rauch <rauchg@gmail.com>
2011-10-16 18:18:44 -0700

    Added configuration based on fs (config.json)

--- index.js
+++ index.js
@@ -11,8 +11,24 @@ var irc = require('irc')
  */
 
 var env = process.env.NODE_ENV || 'development';
+
+/**
+ * Load up configuration.
+ */
+
+var config;
+
+try {
+  config = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf8'));
+} catch (e) {
+  console.error('\033[90mconfig.json unparseable - ignoring\033[39m');
+  config = {
+      server: 'irc.the0th.com'
+    , nickname: 'pwnbot'
+    , channels: ['#pwn']
     , debug: true
-  });
+  };
+}
 
 bot.on('join#pwn', function(who) {
   bot.say('#pwn', who + ': pwned!');
