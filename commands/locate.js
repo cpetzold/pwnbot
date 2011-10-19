fe11d108ef15788fc259361fca55c56081211ff2
Brendan Nee <brendan@Brendans-MacBook-Pro.local>
2011-10-19 01:44:07 -0700

    Moved stuff around to make things work, print errors to console

--- commands/locate.js
+++ commands/locate.js
@@ -12,24 +12,6 @@ var request = require('request')
 
 module.exports = locate;
 
-function checkGeo(element, index, array){
-  if(element.geo && !message){
-    
-    message = 'Last location for @' + text + ': ';
-    
-    //reverse geocode lat lng
-    request({uri:'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + element.geo.coordinates[0] + ',' + element.geo.coordinates[1] + '&sensor=false', json:true}, function (error, response, body) {
-      try{
-        message += body.results[0].formatted_address;
-        message += ' ' + timeago(element.created_at);
-        say(message);
-      } catch (e) {
-        say('Unable to geocode tweet');
-      }
-    });
-  }
-}
-
 /**
  * define command.
  */
@@ -37,22 +19,43 @@ function checkGeo(element, index, array){
 function locate (bot) {
   
   return function locate(text, say, options, user, channel) {
-    var message = '';
     
-    request({uri:'http://search.twitter.com/search.json?q=from:' + text + '&rpp=100', json:true}, function (error, response, body) {
+    function checkGeo(element, index, array){
+      if(element.geo && !message){
+        message = 'Last location for @' + username + ': ';
+
+        //reverse geocode lat lng
+        request({uri:'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + element.geo.coordinates[0] + ',' + element.geo.coordinates[1] + '&sensor=false', json:true}, function (error, response, body) {
+          try{
+            message += body.results[0].formatted_address;
+            message += ' ' + timeago(element.created_at);
+            say(message);
+          } catch (e) {
+            console.error('\033[90m' + e + '\033[39m');
+            say('Unable to geocode tweet');
+          }
+        });
+      }
+    }
+    
+    var message = '';
+    var username = text.toLowerCase();
+    
+    request({uri:'http://search.twitter.com/search.json?q=from:' + username + '&rpp=100', json:true}, function (error, response, body) {
       try{
         if(body.results){
           //there is at least one tweet so loop through all tweets and look for the most recent one with geo info
           body.results.forEach(checkGeo);
           
           if(!message){
-            say(text + ' doesn\'t have any geolocated tweets');
+            say(username + ' doesn\'t have any geolocated tweets');
           }
           
         } else {
-          say(text + ' not found');
+          say(username + ' not found');
         }
       } catch (e) {
+        console.error('\033[90m' + e + '\033[39m');
         say('Unable to reach Twitter');
       }
       
