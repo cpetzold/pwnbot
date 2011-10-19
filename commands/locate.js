f6a459158a6f095e32d8bd6628df9bd88bb41ce1
Brendan Nee <brendan@Brendans-MacBook-Pro.local>
2011-10-19 00:56:12 -0700

    Added try/catch for better error handling

--- commands/locate.js
+++ commands/locate.js
@@ -18,14 +18,13 @@ function checkGeo(element, index, array){
     message = 'Last location for @' + text + ': ';
     
     //reverse geocode lat lng
-    request({uri:'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + element.geo.coordinates[0] + ',' + element.geo.coordinates[1] + '&sensor=false'}, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var location = JSON.parse(body);
-        
-        message += location.results[0].formatted_address;
+    request({uri:'http://maps.googleapis.com/maps/api/geocode/json?latlng=' + element.geo.coordinates[0] + ',' + element.geo.coordinates[1] + '&sensor=false', json:true}, function (error, response, body) {
+      try{
+        message += body.results[0].formatted_address;
         message += ' ' + timeago(element.created_at);
-        
         say(message);
+      } catch (e) {
+        say('Unable to geocode tweet');
       }
     });
   }
@@ -40,26 +39,23 @@ function locate (bot) {
   return function locate(text, say, options, user, channel) {
     var message = '';
     
-    request({uri:'http://search.twitter.com/search.json?q=from:' + text + '&rpp=100'}, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var tweets = JSON.parse(body);
-        
-        if(tweets.results){
+    request({uri:'http://search.twitter.com/search.json?q=from:' + text + '&rpp=100', json:true}, function (error, response, body) {
+      try{
+        if(body.results){
           //there is at least one tweet so loop through all tweets and look for the most recent one with geo info
-          tweets.results.forEach(checkGeo);
+          body.results.forEach(checkGeo);
           
           if(!message){
             say(text + ' doesn\'t have any geolocated tweets');
           }
           
         } else {
-           
           say(text + ' not found');
         }
-      } else {
-        say('Twitter Error');
+      } catch (e) {
+        say('Unable to reach Twitter');
       }
-    
+      
     });
   }
 };
