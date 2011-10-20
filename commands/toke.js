ede039fc70d472856a6029b4584382b14c36aac1
Conner Petzold <cpetzold@gmail.com>
2011-10-19 19:13:22 -0700

    added toke command

---
+++ commands/toke.js
@@ -0,0 +1,44 @@
+/**
+ * Module exports.
+ */
+
+module.exports = toke;
+
+/**
+ * toke command.
+ */
+
+function toke (bot) {
+  
+  return function toke (text, say, options, user, channel) {
+
+      say([
+          '                      dM'
+        , '                      MMr'
+        , '                     4MMML                  .'
+        , '                     MMMMM.                xf'
+        , '     .              "M6MMM               .MM-'
+        , '      Mh..          +MM5MMM            .MMMM'
+        , '      .MMM.         .MMMMML.          MMMMMh'
+        , '       )MMMh.        MM5MMM         MMMMMMM'
+        , '        3MMMMx.     `MMM3MMf      xnMMMMMM"'
+        , '        `*MMMMM      MMMMMM.     nMMMMMMP"'
+        , '          *MMMMMx    "MMM5M.    .MMMMMMM='
+        , '           *MMMMMh   "MMMMM"   JMMMMMMP'
+        , '             MMMMMM   GMMMM.  dMMMMMM            .'
+        , '              MMMMMM  "MMMM  .MMMMM(        .nnMP"'
+        , '   ..          *MMMMx  MMM"  dMMMM"    .nnMMMMM*'
+        , '    "MMn...     `MMMMr `MM   MMM"   .nMMMMMMM*"'
+        , '     "4MMMMnn..   *MMM  MM  MMP"  .dMMMMMMM""'
+        , '       ^MMMMMMMMx.  *ML "M .M*  .MMMMMM**"'
+        , '          *PMMMMMMhn. *x > M  .MMMM**""'
+        , '             ""**MMMMhx/.h/ .=*"'
+        , '                      .3P"%....'
+        , '                    nP"     "*MMnx'
+      ].map(function(s) {
+        return s.irc.green();
+      }).join('\n'));
+      
+  };
+
+};
