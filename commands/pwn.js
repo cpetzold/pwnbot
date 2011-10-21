d200df319701a05638b6b2ecbfc7c81c4df02d75
Brendan Nee <brendan.nee@gmail.com>
2011-10-20 20:09:54 -0700

    Added support for multipwn:
    example:
    !pwn guillermo|5

--- commands/pwn.js
+++ commands/pwn.js
@@ -13,34 +13,54 @@ function pwn (bot) {
   return function pwn (text, say, options, user, channel) {
     var usernames = text.split(' ');
     usernames.forEach(function(username, index, array){
-      //check if user is in room
-      bot.whois(username, function pwnUser(info){
-        var message = '';
-        console.log(info);
+      //see if a pwn quantity is present
+      // example !pwn gullermo|10
+      args = username.split('|');
+      if(args.length > 1){
+        username = args[0];
+        var pwnQuantity = args[1];
+        var pwnInterval = setInterval(pwnUser, 1000); 
+      } else {
+        pwnUser();
+      }
       
-        if(!info){
-          message = username + ' isn\'t on this channel.  Unable to pwn.';
+      var pwnCount = 0;
+      
+      function pwnUser(){
+        //check if user is in room
+        bot.whois(username, function(info){
+          var message = '';
+          console.log(info);
+      
+          if(!info){
+            message = username + ' isn\'t on this channel.  Unable to pwn.';
         
-        } else {
-        
-          if(username == user){
-            //check that user isn't the same as self
-            message = username + ' pwned themselves.';
-          
-          } else if(username == bot.config.nickname){
-            //you can't pwn the pwnbot
-            message = 'You can\'t pwn the pwnbot.';
-          
           } else {
-            //try to pwn the user
-            message = info.nick + ' (' + info.realname + ') has been pwned.';
+        
+            if(username == user){
+              //check that user isn't the same as self
+              message = username + ' pwned themselves.';
           
-          }
+            } else if(username == bot.config.nickname){
+              //you can't pwn the pwnbot
+              message = 'You can\'t pwn the pwnbot.';
+          
+            } else {
+              //try to pwn the user
+              message = info.nick + ' (' + info.realname + ') has been pwned.';
+          
+            }
        
-        }
-        say(message);
+          }
+          say(message);
+          pwnCount++;
+          if(pwnCount >= pwnQuantity){
+            clearInterval(pwnInterval);
+          }
       
-      });
+        });
+        
+      }
       
     });
   };
