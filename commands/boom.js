e7861be4ccbcd2d6afd31ac198964f8919886b57
Brendan Nee <brendan.nee@gmail.com>
2011-10-24 23:44:32 -0700

    Added support for a boom queue with max of one.

--- commands/boom.js
+++ commands/boom.js
@@ -11,12 +11,20 @@ module.exports = boom;
 function boom (bot) {
   
   return function boom (text, say, options, user, channel) {
+    
+    var boom = this.boom
     var message = 'BOOM!'.irc.red();
     
-    setTimeout(boom, (Math.random()*30000 + 2000));
-    
-    function boom(){
-      say(message);
+    if(boom.boomOn){
+      say('Error:'.irc.red() + ' Boom queue full.'.irc.gray());
+    } else {
+      boom.boomOn = true;
+      setTimeout(doBoom, (Math.random()*30000 + 2000));
+
+      function doBoom(){
+        boom.boomOn = false;
+        say(message);
+      }
     }
   };
 
